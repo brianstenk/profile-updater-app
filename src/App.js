10982a05cb07a962867bfce35c3abba1260a9469
Brian N <nkurunungib@gmail.com>
2022-10-22 18:21:12 -0500

    Added axios to bring the data to the front end

--- src/App.js
+++ src/App.js
@@ -1,12 +1,42 @@
-import logo from './logo.svg';
+
 import './App.css';
 
+import axios from 'axios'
+
+import {React, useEffect, useState} from 'react';
+
+
+const UserProfile = () => {
+
+  const fetchUserProfiles = () => {
+    const url = "http://localhost:8080/api/v1/userProfiles";
+    
+    axios.get(url).then(
+      resp => console.log(resp)
+    );
+    
+  }
+  
+  useEffect (
+    () => {
+      fetchUserProfiles();
+    }, []
+  );
+  
+}
+
 function App() {
+
   return (
     <div className="App">
-      <h1>Test set up</h1>
+      <h1>List of UserProfile</h1>
+
+      <UserProfile />
+
     </div>
   );
 }
+
+
 
 export default App;

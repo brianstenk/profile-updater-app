2f5700daa6da6d4249382b272aa4cc34978897b1
Brian N <nkurunungib@gmail.com>
2022-10-23 20:58:56 -0500

    Added state and err handling

--- src/App.js
+++ src/App.js
@@ -3,25 +3,89 @@ import './App.css';
 
 import axios from 'axios'
 
-import {React, useEffect, useState} from 'react';
+import {React, useEffect, useState, useCallback} from 'react';
+
+import {useDropzone} from 'react-dropzone';
 
 
 const UserProfile = () => {
 
-  const fetchUserProfiles = () => {
-    const url = "http://localhost:8080/api/v1/userProfiles";
-    
-    axios.get(url).then(
-      resp => console.log(resp)
-    );
-    
-  }
-  
+  const[userProfiles, setUserProfiles] = useState([]);
+
   useEffect (
-    () => {
+     () => {
+
+      const fetchUserProfiles = () => {
+
+        const url = "http://localhost:8080/api/v1/userProfiles";
+        
+        axios.get(url).then(
+          resp => {
+            console.log(resp.data)
+            const data = resp.data;
+            setUserProfiles(data);
+    
+          }
+        ).catch(
+          err => console.log(err)
+        );
+        
+      };
+
       fetchUserProfiles();
     }, []
   );
+
+  function MyDropzone({profileId}) {
+
+    const onDrop = useCallback(acceptedFiles => {
+      // Do something with the files
+      const file = acceptedFiles[0];
+
+      console.log(file);
+
+      const formData = new FormData();
+      formData.append("file", file);
+
+      console.log("The prof Id ", profileId)
+
+      axios.post(`http://localhost:8080/api/v1/userProfiles/${profileId}/image/upload`, 
+      formData,
+      {
+        headers: {
+          "Content-Type": "multipart/form-data"
+        }
+      })
+      .then(() => console.log("file uploaded successfully"))
+      .catch(err => console.log(err));
+
+    }, []);
+
+    const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+  
+    return (
+      <div {...getRootProps()}>
+        <input {...getInputProps()} />
+        {
+          isDragActive ?
+            <p>Drop the image here ...</p> :
+            <p>Drag 'n' drop some image here, or click to select image</p>
+        }
+      </div>
+    )
+  }
+
+  return userProfiles.map((userProfile, index) => {
+    return <div key = {index}>
+        <br/>
+         <h2>{userProfile.username}</h2>
+         <p>{userProfile.profileId}</p>
+         <MyDropzone {...userProfile}/>
+         {/* <MyDropzone userProfileId = {userProfile.profileId}/> */}
+         <br/>
+    </div>
+ 
+  })
   
 }
 
@@ -30,6 +94,8 @@ function App() {
   return (
     <div className="App">
       <h1>List of UserProfile</h1>
+
+      {/* map the state into what we we want */}
 
       <UserProfile />
 
